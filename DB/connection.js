420b26d73152b8e543a79b0754b3ed6f43d640ad
Areeb shakeel <areebshakeel>
2021-01-23 01:03:35 +0500

    heruko app basic setup done

--- DB/connection.js
+++ DB/connection.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const URI='mongodb+srv://dbArif:testpassword123@cluster0.pzwai.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const URI='mongodb+srv://dbArif:arifshakeel@cluster0.pzwai.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
 const connectDB=async ()=>{
    await mongoose.connect(URI,{ useUnifiedTopology: true },{ useNewUrlParser: true })
    console.log('DB connect!')
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express=require('express');
 const app=express();
+const route=express.Router();
 const connectDB=require('./DB/connection')
+const User=require('./Modal/user')
+const { MongoClient } = require("mongodb");
+const URI='mongodb+srv://dbArif:arifshakeel@cluster0.pzwai.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const client = new MongoClient(URI);
 connectDB();
-const port=process.env.PORT||5000
-app.get('/user',(req,res)=>{
-res.send('Hello world')
+
+client.connect().then(res=>{
+
+    const database = client.db("table-reservation");
+    const collection = database.collection("user");
+    app.get('/user',async(req,res)=>{
+        const test=await collection.findOne({firstName:"Arif"})
+        console.log(test)
+         res.send(test)
+        })
 })
+
+const port=process.env.PORT||5000
+
 // app.use('/api/userModal',require('./endPoint/user'))
 app.listen(port,()=>{
     console.log('PORT START AT',port)
